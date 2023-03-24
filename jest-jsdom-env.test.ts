966114b9e6e26e4bfdd0075acaa7e107a5a5169c
Markus Schulte <mail@schulte-development.de>
2023-03-24 17:01:09 +0100

    Using jest.replaceProperty

--- jest-jsdom-env.test.ts
+++ jest-jsdom-env.test.ts
@@ -5,4 +5,7 @@
 test('use jsdom in this test file', () => {
   const element = document.createElement('div');
   expect(element).not.toBeNull();
+
+  jest.replaceProperty(element, "dir", "my-dir");
+  expect(element.dir).toBe("my-dir")
 });
